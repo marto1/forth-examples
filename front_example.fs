4d2f8d4d54dd210f5ff04e7e9582b08d6a3cff1c
Martin Gergov <martingergov1@gmail.com>
2016-09-29 13:43:02 +0200

    attributes convention added and embeding issue found

--- front_example.fs
+++ front_example.fs
@@ -2,7 +2,11 @@ html
   head
     s"Example application" title
     s"utf-8" charset meta
-  body
+  body \ embedded in html BUT not in head
     10 10 grid
-      1 x 1 y s"Hello there" value button
-      2 x 2 y 10 maxlen input
+      1 'x 1 'y s"Hello there" 'value button \ attributes begin with '
+      2 'x 2 'y 10 'maxlen input
+      div \ sequence
+      div
+      div
+      div div div \ embedded
