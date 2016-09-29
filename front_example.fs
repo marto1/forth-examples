0f58351c70d35beca1382bb43a692869d4c15c3b
Martin Gergov <martingergov1@gmail.com>
2016-09-30 01:40:48 +0200

    should be the minimal syntax required for most html/xml stuff

--- front_example.fs
+++ front_example.fs
@@ -1,12 +1,14 @@
-html
-  head
+html << \ << after a tag means next tag is embedded
+  head <<
     s"Example application" title
-    s"utf-8" charset meta
-  body \ embedded in html BUT not in head
+    s"utf-8" 'charset meta
+  body <<
     10 10 grid
-      1 'x 1 'y s"Hello there" 'value button \ attributes begin with '
+      1 'x 1 'y s"Hello there" 'value button \ attributes begin with ' and come before tag
       2 'x 2 'y 10 'maxlen input
-      div \ sequence
       div
       div
-      div div div \ embedded
+      div
+      div << div << div \ embedding
+      >> >> \ going 2 levels out
+      div \ this div will be clild of grid
